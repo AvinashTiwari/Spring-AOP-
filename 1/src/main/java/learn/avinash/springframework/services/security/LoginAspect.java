636a2140559e73bf685f03bae9d2b4ebd1dc7e69
Avinash Tiwari <qwe123kids@gmail.com>
2018-04-14 15:26:06 -0600

    Login event

--- 1/src/main/java/learn/avinash/springframework/services/security/LoginAspect.java
+++ 1/src/main/java/learn/avinash/springframework/services/security/LoginAspect.java
@@ -1,15 +1,20 @@
 package learn.avinash.springframework.services.security;
 
 import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-/**
- * Created by jt on 1/6/16.
- */
 @Aspect
 @Component
 public class LoginAspect {
+
+    private LoginFailureEventPublisher publisher;
+
+    @Autowired
+    public void setPublisher(LoginFailureEventPublisher publisher) {
+        this.publisher = publisher;
+    }
 
     @Pointcut("execution(* org.springframework.security.authentication.AuthenticationProvider.authenticate(..))")
     public void doAuthenticate(){
@@ -32,5 +37,8 @@ public class LoginAspect {
     public void logAuthenicationException(Authentication authentication){
         String userDetails = (String) authentication.getPrincipal();
         System.out.println("Login failed for user: " + userDetails);
+
+        publisher.publish(new LoginFailureEvent(authentication));
+
     }
 }
